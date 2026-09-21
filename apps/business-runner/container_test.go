712f600fe8d5698ea0ab7cc983d346862a87bd73
heavybullets8 <heavybullets8@gmail.com>
2026-09-20 20:30:43 -0600

    Preinstall database backup tools for unprivileged CI

--- apps/business-runner/container_test.go
+++ apps/business-runner/container_test.go
@@ -21,6 +21,11 @@ getent group docker | grep -q ':123:'
 test -x /home/runner/run.sh
 test -d /home/runner/externals
 command -v sudo
-pdftotext -v`,
+pdftotext -v
+pg_dump --version | grep -E 'PostgreSQL\) 17\.'
+pg_restore --version | grep -E 'PostgreSQL\) 17\.'
+pg_isready --version
+aws --version
+age --version`,
 	)
 }
