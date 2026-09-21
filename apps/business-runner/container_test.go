dba785787cba8f40f45363f24b920dbc4296058c
heavybullets8 <heavybullets8@gmail.com>
2026-09-20 18:47:03 -0600

    feat: add Poppler business runner image

---
+++ apps/business-runner/container_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	helpers "github.com/heavybullets8/containers/tests"
+)
+
+func TestRunnerContract(t *testing.T) {
+	image := helpers.GetTestImage("ghcr.io/heavybullets8/business-runner:rolling")
+	helpers.RequireCommandSucceeds(
+		t,
+		image,
+		nil,
+		"/bin/sh",
+		"-lc",
+		`test "$(id -u)" = 1001
+test "$(id -g)" = 1001
+getent group docker | grep -q ':123:'
+test -x /home/runner/run.sh
+test -d /home/runner/externals
+command -v sudo
+pdftotext -v`,
+	)
+}
